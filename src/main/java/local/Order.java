fcf3185b133e80f2469375c45ab3cbe13a2acfc0
picturesque0930 <picturesque0930@sk.com>
2020-07-28 15:46:23 +0900

    Init

---
+++ src/main/java/local/Order.java
@@ -0,0 +1,71 @@
+package local;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long productId;
+    private Integer qty;
+
+    @PostPersist
+    public void onPostPersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+
+
+    }
+
+    @PreRemove
+    public void onPreRemove(){
+        OrderCanceled orderCanceled = new OrderCanceled();
+        BeanUtils.copyProperties(this, orderCanceled);
+        orderCanceled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        local.external.Cancelation cancelation = new local.external.Cancelation();
+        cancelation.setOrderId(this.getId());
+        cancelation.setStatus("status canceld");
+        // mappings goes here
+        OrderApplication.applicationContext.getBean(local.external.CancelationService.class)
+            .cancel(cancelation);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+
+
+
+}
